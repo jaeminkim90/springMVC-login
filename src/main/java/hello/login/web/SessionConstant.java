7addb0c5947cf029824100cca90b856807620575
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-16 18:52:56 +0900

    FEAT: HttpServletRequest가 제공하는 session 기능을 이용해 로그인 세션 구현

--- src/main/java/hello/login/web/SessionConstant.java
+++ src/main/java/hello/login/web/SessionConstant.java
@@ -2,5 +2,6 @@ package hello.login.web;
 
 // 상수만 사용할 목적으로 만든 클래스
 public class SessionConstant {
-    private static final String LOGIN_MEMBER = "loginMember";
+    public static final String LOGIN_MEMBER = "loginMember";
+
 }
