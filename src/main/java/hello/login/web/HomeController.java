8df56a7645258c829113f6bb3c2a7fc8478c33e9
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-17 11:56:13 +0900

    FEAT: @SessionAttribute를 이용하여 로그인 상태를 유지하는  세션 기능 구현

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -65,7 +66,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    // @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         // 로그인 화면에서 세션이 생성되지 않도록 false 처리
@@ -85,6 +86,20 @@ public class HomeController {
         // 세션이 유지되면 로그인으로 이동
         model.addAttribute("member", loginMember);
         return "loginHome";
+    }
 
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            // @SesseionAttribute는 속성에 해당하는 세션을 가지고 있는 Member 객체를 꺼낸다
+            @SessionAttribute(name = SessionConstant.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // 세션에 회원 데이터가 없으면 home으로 이동
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
